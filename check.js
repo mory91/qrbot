4cf22cbbb3704fbc7ea273608f1329ba1d81b52d
mortezaho.1376@gmail.com <mortezaho.1376@gmail.com>
2017-12-10 15:13:39 +0330

    debug2

--- check.js
+++ check.js
@@ -8,7 +8,9 @@ const Qr = require('./model/message')
 let nums = {}
 Qr.find({}, function(err, res) {
     for(let r in res) {
-        nums[r.chatId] = 0 | (nums[r.chatId] + 1);
+        if (!nums[r.chatId])
+            nums[r.chatId] = 0
+        nums[r.chatId] = (nums[r.chatId] + 1);
     }
     console.log(nums)
 })
